26aa2fa62c1022a2081a844d8674d38aedaa7125
JeanDevFR <castellotti.j@gmail.com>
2021-12-28 18:22:36 +0100

    Test

--- app/oauth2.py
+++ app/oauth2.py
@@ -36,11 +36,14 @@ def verify_jwt_token(token: str, login_exception: HTTPException):
     return token
 
 
-def get_current_user(token: str = Depends(oauth2_scheme),
-                     db: Session = Depends(database.get_db)):
-    login_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                    detail="Could not validate credentials",
-                                    headers={"WWW-Authenticate": "Bearer"})
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)
+):
+    login_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     payload = verify_jwt_token(token, login_exception)
     user = db.query(models.User).filter(models.User.id == payload.id).first()
     return user
