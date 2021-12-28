1c4343dad26413ab8d3eb3661ef37a2d3f3f550b
JeanDevFR <castellotti.j@gmail.com>
2021-12-28 16:50:20 +0100

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
-from . import models
-from .database import engine
+
+# from . import models
+# from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 # Database Creation
 # models.Base.metadata.create_all(bind=engine)
 
 # FastAPI Instance Creation
 app = FastAPI()
+
+# origins = ["https://www.google.fr"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Routes
 app.include_router(post.router)
