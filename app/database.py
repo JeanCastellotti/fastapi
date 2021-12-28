8cffc9fadc1034602b4925fe3e8fe6789ab57c30
JeanDevFR <castellotti.j@gmail.com>
2021-12-28 18:49:22 +0100

    Fix typo

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-engine = create_engine(settings.database_uri.replace("postgres://", "postgresql://"))
+engine = create_engine(settings.database_url.replace("postgres://", "postgresql://"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
