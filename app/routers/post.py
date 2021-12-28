604da4daf1c6761a30cc9fbf2e7010ddf0d90dcd
JeanDevFR <castellotti.j@gmail.com>
2021-12-28 16:51:31 +0100

    Fix bug: wrong schema

--- app/routers/post.py
+++ app/routers/post.py
@@ -44,7 +44,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 @router.post("/",
              status_code=status.HTTP_201_CREATED,
-             response_model=schemas.PostOut)
+             response_model=schemas.Post)
 def create_post(post: schemas.PostCreate,
                 db: Session = Depends(get_db),
                 current_user: models.User = Depends(oauth2.get_current_user)):
